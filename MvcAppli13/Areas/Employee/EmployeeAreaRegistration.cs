8fd555e5cd6db0b9dccc35a1a14fc397ddd60b20
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 17:03:39 +0530

    MvcAppli13

---
+++ MvcAppli13/Areas/Employee/EmployeeAreaRegistration.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+
+namespace MvcAppli13.Areas.Employee
+{
+    public class EmployeeAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Employee";
+            }
+        }
+
+        //public override void RegisterArea(AreaRegistrationContext context)
+        //{
+        //    context.MapRoute(
+        //        "Employee_default",
+        //        "Employee/{controller}/{action}/{id}",
+        //        new { action = "Index", id = UrlParameter.Optional }
+        //    );
+        //}
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Employee_default",
+                "Employee/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+
+
+    }
+}
